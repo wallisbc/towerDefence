e512d7fe11fa2338e12e9653839f179c5c8378be
Brendan Wallis <brendan.c.wallis@gmail.com>
2016-06-03 11:02:53 +1000

    Add files via upload
    code files plus assets

---
+++ EnemyDestructionDelegate.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyDestructionDelegate : MonoBehaviour {
+
+    public delegate void EnemyDelegate(GameObject enemy);
+    public EnemyDelegate enemyDelegate;
+
+    void OnDestroy()
+    {
+        if (enemyDelegate != null)
+        {
+            enemyDelegate(gameObject);
+        }
+    }
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
